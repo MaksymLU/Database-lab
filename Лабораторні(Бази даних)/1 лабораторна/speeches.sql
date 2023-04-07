fcd4cdc93344786df531df167da77ab17433f9de
MaksymLU <109483747+MaksymLU@users.noreply.github.com>
2023-04-07 22:50:44 +0300

    5 lab UPDATED

---
+++ Лабораторні(Бази даних)/1 лабораторна/speeches.sql
@@ -0,0 +1,22 @@
+INSERT INTO public.speeches (topic, speaker_id, section_id, start_time, duration, equipment_needed, office_fk)
+VALUES
+  ('The Importance of Cybersecurity in the Digital Age', 11, 3, '2022-11-10 10:00:00', '01:30:00', 1, 2),
+  ('The Future of Transportation: Trends and Innovations', 12, 4, '2022-12-05 14:00:00', '01:00:00', 3, 1),
+  ('The Role of Technology in Education', 13, 1, '2023-01-15 11:00:00', '01:15:00', NULL, 3),
+  ('The Business Case for Sustainability', 14, 5, '2023-02-20 13:00:00', '01:30:00', 5, 2),
+  ('The Power of Emotional Intelligence in Leadership', 15, 2, '2023-03-10 09:00:00', '01:00:00', NULL, 1);
+INSERT INTO public.speeches (topic, speaker_id, section_id, start_time, duration, equipment_needed, office_fk)
+VALUES
+  ('The Importance of Data Science', 1, 2, '2022-01-01 10:00:00', '01:00:00', 3, 1),
+  ('The Future of Artificial Intelligence', 2, 1, '2022-02-15 14:30:00', '00:45:00', NULL, 2),
+  ('Innovation in the Energy Sector', 3, 4, '2022-03-20 09:00:00', '01:30:00', 2, 3),
+  ('Digital Transformation in Healthcare', 4, 3, '2022-04-10 11:00:00', '01:15:00', NULL, 1),
+  ('The Role of Women in Leadership', 5, 5, '2022-05-05 13:00:00', '01:30:00', 1, 2);
+INSERT INTO public.speeches (topic, speaker_id, section_id, start_time, duration, equipment_needed, office_fk)
+VALUES
+  ('The Ethics of AI in Finance', 6, 1, '2022-06-01 15:00:00', '01:15:00', 4, 3),
+  ('Sustainable Agriculture: The Future of Farming', 7, 4, '2022-07-15 11:30:00', '01:30:00', 5, 2),
+  ('The Future of Work: Trends and Challenges', 8, 5, '2022-08-10 09:00:00', '01:00:00', NULL, 1),
+  ('How Technology is Changing the Entertainment Industry', 9, 3, '2022-09-05 14:00:00', '01:30:00', 6, 1),
+  ('The Power of Diversity and Inclusion in the Workplace', 10, 2, '2022-10-20 16:00:00', '01:15:00', 2, 3);
+
